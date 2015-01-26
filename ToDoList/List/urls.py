467326b93a96064b6da621d1f0f4cf8f5d40b4ed
arifsundrani <arifsundrani@Arifs-MacBook-Air.local>
2015-01-26 17:29:39 -0500

    Updated transition
    fixed issue with transitioning from sign in/sign up page to list page

--- ToDoList/List/urls.py
+++ ToDoList/List/urls.py
@@ -3,4 +3,5 @@ from List import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
+	url('list/$', views.list, name='list'),
 )
--- ToDoList/List/views.py
+++ ToDoList/List/views.py
@@ -4,6 +4,14 @@ from django.template import RequestContext, loader
 
 from List.models import Task
 # Create your views here.
+def list(request):
+	todo_list = Task.objects.all()
+	template = loader.get_template('List/list.html')
+	context = RequestContext(request, {
+		'todo_list': todo_list,
+	})
+	#output = '\n '.join([t.task_text for t in todo_list])
+	return HttpResponse(template.render(context))
 def index(request):
 	todo_list = Task.objects.all()
 	context = {'ToDo_List':todo_list}
