28dc59580c70fa32bdf59498418b951751369ed3
arifsundrani <arifsundrani@Arifs-MacBook-Air.local>
2015-01-26 16:01:06 -0500

    Connecting to html
    added urls,views, and index.html

--- ToDoList/List/views.py
+++ ToDoList/List/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
+from List.models import Task
 # Create your views here.
+def index(request):
+	todo_list = Task.objects.all()
+	context = {'ToDo_List':todo_list}
+	return render(request, 'List/index.html', context)
--- ToDoList/ToDoList/urls.py
+++ ToDoList/ToDoList/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ToDoList.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+	url(r'^List/', include('List.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
