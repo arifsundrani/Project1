9fd25ac48857e7292aec083277c1914bb4f4061c
Deh Jun Tzou <dtzou3@emory.edu>
2015-01-27 15:28:37 -0500

    attempted user authentication function

--- ToDoList/List/views.py
+++ ToDoList/List/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404, render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
-from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.core.context_processors import csrf
+
 
 
 from List.models import Task
@@ -51,3 +53,37 @@ def request_page(request):
   if(request.POST.get('btn')):
   	    Todo.objects.get(id=todo_id).completed = True    
   return render('List/list.html')
+
+
+
+
+
+# for user authentication
+
+def login(request):
+	c = {}
+	c.update(csrf(request))
+	return render_to_response('login.html', c)
+
+def auth_view(request):
+	username = request.POST.get('username', '')
+	password = request.POST.get('password', '')
+	user = auth.authenticate(username=username, password=password)
+	
+	if user is not None:
+		auth.login(request, user)
+		return HttpResponseRedirect('/accounts/loggedin')
+	else:
+		return HttpResponseRedirect('/accounts/invalid')
+
+def loggedin(request):
+	return render_to_response('loggedin.html', {'full_name': request.user.username})
+
+def invalid_login(reqeust):
+	return render_to_response('invalid_login.html')
+
+def logout(request):
+	auth.logout(request)
+	return render_to_response('logout.html')
+
+
--- ToDoList/ToDoList/urls.py
+++ ToDoList/ToDoList/urls.py
@@ -9,10 +9,10 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     
     
-   # url(r'^accounts/login/$', 'List.views.login'),
-    #url(r'^accounts/auth/$', 'List.views.auth_view'),
-    #url(r'^accounts/logout/$', 'List.views.logout'),
-    #url(r'^accounts/loggedin/$', 'List.views.loggedin'),
-    #url(r'^accounts/invalid/$', 'List.views.invalid_login'),
+   url(r'^accounts/login/$', 'ToDoList.views.login'),
+    url(r'^accounts/auth/$', 'ToDoList.views.auth_view'),
+    url(r'^accounts/logout/$', 'ToDoList.views.logout'),
+    url(r'^accounts/loggedin/$', 'ToDoList.views.loggedin'),
+    url(r'^accounts/invalid/$', 'ToDoList.views.invalid_login'),
     
 )
