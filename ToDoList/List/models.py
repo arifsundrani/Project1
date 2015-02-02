a0d17d4947072b049b8386f8871cd7f4a026a383
Deh Jun Tzou <dtzou3@emory.edu>
2015-02-02 12:58:51 -0500

    commit

--- ToDoList/List/models.py
+++ ToDoList/List/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-	task_text = models.CharField(max_length=1000)
+	task_text = models.CharField(max_length=1000, unique=True)
 	completed = models.BooleanField(default=False)
 	def __str__(self):
 		return self.task_text
